0956368b51329738b2fddef1812785dc7cdc1432
Sergoot <sergey.lyutavin22@mail.ru>
2024-10-12 16:54:48 +0300

    chore: configs and readme

---
+++ bff/bff/main/config/__init__.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    
+    encashment_host: str
+    algo_host: str
+    
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra = "allow",
+        env_prefix = "BFF_",
+    )
