9a7d7a232d724329c8272aefe3ae4a5ddbcb19c2
slava <slavabudarin@gmail.com>
2024-10-29 01:15:10 +0300

    скрипт по выгрузке банкоматов с фильтрацией по МКАДу

---
+++ scripts/ATM_to_BD/atms_to_db.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from scripts.Utils.PSQLutils import PSQL
+from scripts.Utils.OSMutils import OSMatms
+
+
+sql = PSQL()
+os = OSMatms()
+
+atms = os.get_city()
+atms = pd.DataFrame(atms)
+atms = atms.fillna('Неизвестно')
+for index, atm in atms.iterrows():
+    osmid = atm['osmid']
+    lon = atm['lon']
+    lat = atm['lat']
+    operator = atm['operator']
+    in_MKAD = atm['in_MKAD']
+    sql.write_atm_to_db(osmid, lon, lat, operator, in_MKAD)
+sql.close()
+
+
+
+
