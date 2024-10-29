b63cd50364f3e19af12039b537956c15d34a5a61
slava <slavabudarin@gmail.com>
2024-10-29 11:59:44 +0300

    переписал код в соответствии с вынесенной функцией

--- scripts/ATM_to_BD/atms_to_map.py
+++ scripts/ATM_to_BD/atms_to_map.py
@@ -1,23 +1,12 @@
-import folium
 import pandas as pd
 from scripts.Utils.PSQLutils import PSQL
-
+from scripts.Utils.MapUtils.FoliumUtils import ToFoliumMap
 sql = PSQL()
 lol = sql.get_atms_from_db(in_MKAD = True)
 ATM_df = pd.DataFrame(lol, columns=['osmid', 'lon', 'lat', 'operator', 'in_MKAD'])
 
 
 output_file = "output_htmls/atms_on_map.html"
-map_center=[55.751244, 37.618423]
-zoom_start=12
-
-map_ = folium.Map(location=map_center, zoom_start=zoom_start)
-
-for index, row in ATM_df.iterrows():
-
-    lat = row['lat']
-    lon = row['lon']
-    folium.Marker(location=[lat, lon]).add_to(map_)
-
-map_.save(output_file)
-print(f"Карта сохранена как {output_file}")
+To_map = ToFoliumMap()
+To_map.generate_map(ATM_df)
+To_map.save_map(output_file)
--- scripts/ATM_to_BD/mkad_to_map.py
+++ scripts/ATM_to_BD/mkad_to_map.py
@@ -2,23 +2,14 @@
 простой скрипт по отрисовке всех данных точек МКАДа
 из которых потом строится граница для АТМок
 """
-import folium
 import pandas as pd
-from scripts.Utils.OSMutils.mkad_coords import MKAD_1
+from scripts.Utils.MapUtils.mkad_coords import MKAD_1
+from scripts.Utils.MapUtils.FoliumUtils import ToFoliumMap
 
 MKAD_df = pd.DataFrame(MKAD_1, columns=['lon', 'lat'])
 
 output_file = "output_htmls/mkad_dots_on_map.html"
-map_center=[55.751244, 37.618423]
-zoom_start=12
 
-map_ = folium.Map(location=map_center, zoom_start=zoom_start)
-
-for index, row in MKAD_df.iterrows():
-
-    lat = row['lat']
-    lon = row['lon']
-    folium.Marker(location=[lat, lon]).add_to(map_)
-
-map_.save(output_file)
-print(f"Карта сохранена как {output_file}")
+To_map = ToFoliumMap()
+To_map.generate_map(MKAD_df)
+To_map.save_map(output_file)
