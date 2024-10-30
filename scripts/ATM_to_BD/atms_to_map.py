ca4f475c244322c6a42f7338be46ead524e11501
slava <slavabudarin@gmail.com>
2024-10-30 22:38:41 +0300

    мелкие изменения связанные с работой импортированных классов

--- scripts/ATM_to_BD/atms_to_map.py
+++ scripts/ATM_to_BD/atms_to_map.py
@@ -5,14 +5,11 @@ from scripts.Utils.MapUtils.FoliumUtils import ToFoliumMap
 from scripts.Utils.PSQLutils.config import ServerConf, TableATM
 
 sql = PSQL(ServerConf, TableATM)
-sql.connect()
 lol = sql.fetch_all_rows("in_mkad=TRUE")
 sql.close()
-ATM_df = pd.DataFrame(lol, columns=['osmid', 'lon', 'lat', 'operator', 'in_MKAD'])
-print(sql.last_query)
-print(ATM_df)
+ATM_df = pd.DataFrame(lol, columns=TableATM.table_columns)
 
 output_file = "output_htmls/atms_on_map.html"
 To_map = ToFoliumMap()
-To_map.generate_map(ATM_df)
+To_map.generate_map(ATM_df,lat_key='x_lon',lon_key='y_lat')
 To_map.save_map(output_file)
