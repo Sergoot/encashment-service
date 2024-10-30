1adf9c2c9a2ee3400be1316cf7bea8338c1e59ca
slava <slavabudarin@gmail.com>
2024-10-30 22:39:32 +0300

    сделал более универсальной, вынес ключи широты и долготы в отдельные параметры

--- scripts/Utils/MapUtils/FoliumUtils.py
+++ scripts/Utils/MapUtils/FoliumUtils.py
@@ -8,10 +8,10 @@ class ToFoliumMap:
                  zoom_start:int = 12):
         self.map = folium.Map(location=map_center, zoom_start=zoom_start)
 
-    def generate_map(self, coordinates:DataFrame):
+    def generate_map(self, coordinates:DataFrame, lat_key, lon_key):
         for index, row in coordinates.iterrows():
-            lat = row['lat']
-            lon = row['lon']
+            lat = row[lat_key]
+            lon = row[lon_key]
             folium.Marker(location=[lat, lon]).add_to(self.map)
 
     def save_map(self, outputfile:str):
