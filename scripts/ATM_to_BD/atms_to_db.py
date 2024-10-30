804225cc241609ddf89815fb5ee59b5e8f209381
slava <slavabudarin@gmail.com>
2024-10-30 22:59:57 +0300

    переписал в соответствии с обновленным классом постгри

--- scripts/ATM_to_BD/atms_to_db.py
+++ scripts/ATM_to_BD/atms_to_db.py
@@ -1,21 +1,27 @@
 import pandas as pd
+from numpy.random.mtrand import operator
+
 from scripts.Utils.PSQLutils import PSQL
 from scripts.Utils.MapUtils.OSMUtils import OSMatms
 
+from scripts.Utils.PSQLutils.config import ServerConf, TableATM
 
-sql = PSQL()
+sql = PSQL(ServerConf, TableATM)
+#nearest_nodes = PSQL(dbname = 'neare
+# st_nodes')
 os = OSMatms()
 
 atms = os.get_city()
 atms = pd.DataFrame(atms)
 atms = atms.fillna('Неизвестно')
 for index, atm in atms.iterrows():
-    osmid = atm['osmid']
-    lon = atm['lon']
-    lat = atm['lat']
-    operator = atm['operator']
-    in_MKAD = atm['in_MKAD']
-    sql.write_atm_to_db(osmid, lon, lat, operator, in_MKAD)
+    sql.insert_row(osmid=atm['osmid'],
+                   x_lon=atm['lon'],
+                   y_lat=atm['lat'],
+                   operator=atm['operator'],
+                   in_mkad= atm['in_MKAD'],
+                   on_conflict_ignore=True
+                   )
 sql.close()
 
 
