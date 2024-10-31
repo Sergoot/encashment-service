7d50bbb2548d5312c28ba96af96b5a896bcf052e
davidchulakov <chulakov008@yandex.ru>
2024-10-31 20:26:26 +0300

    Добавил создание таблицы

--- atm/src/database.py
+++ atm/src/database.py
@@ -13,6 +13,11 @@ async_session = async_sessionmaker(
     engine, expire_on_commit=False
 )
 
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
 async def get_session():
     async with async_session() as session:
         yield session
