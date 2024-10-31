fccd174d1bd198e56404664d8382c88eb24d8c7d
slava <slavabudarin@gmail.com>
2024-10-31 20:13:56 +0300

    небольшой скрипт по мёрджу 2 таблиц для упрощения анализа данных

---
+++ scripts/ATM_to_BD/tables_to_csv.py
@@ -0,0 +1,14 @@
+
+from scripts.Utils.PSQLutils.main import PSQL
+from scripts.Utils.PSQLutils.config import TableATM,TableNearest, ServerConf
+import pandas as pd
+atms_sql = PSQL(ServerConf, TableATM)
+nn_sql = PSQL(ServerConf, TableNearest)
+
+atms = atms_sql.fetch_all_rows()
+nn = nn_sql.fetch_all_rows()
+atms = pd.DataFrame(atms, columns=['atm_osmid' , 'atm_lon', 'atm_lat' , 'operator', 'atm_in_mkad'])
+nn = pd.DataFrame(nn, columns=['nn_osmid', 'atm_osmid' , 'nn_lon', 'nn_lat' , 'distance', 'nn_in_mkad'])
+
+merged = pd.merge(atms, nn, on='atm_osmid')
+merged.to_csv('output_csv/merged_atms+nn.csv')
