4b1e00d92415b90f6da886f1f41eb9534a81cc56
slava <slavabudarin@gmail.com>
2024-11-01 10:03:57 +0300

    добавил возможность генерации АТМов и стрелок до ближайших обочин

--- scripts/Utils/MapUtils/FoliumUtils.py
+++ scripts/Utils/MapUtils/FoliumUtils.py
@@ -8,11 +8,28 @@ class ToFoliumMap:
                  zoom_start:int = 12):
         self.map = folium.Map(location=map_center, zoom_start=zoom_start)
 
-    def generate_map(self, coordinates:DataFrame, lat_key, lon_key):
+    def generate_atm_map(self, coordinates:DataFrame, lat_key, lon_key):
         for index, row in coordinates.iterrows():
             lat = row[lat_key]
             lon = row[lon_key]
             folium.Marker(location=[lat, lon]).add_to(self.map)
+
+    def generate_atm_nn_map(self, coordinates:DataFrame,
+                            atm_lat_key:str,
+                            atm_lon_key:str,
+                            nn_lat_key:str,
+                            nn_lon_key:str
+                            ):
+        for index, row in coordinates.iterrows():
+            atm_coord = [row[atm_lat_key], row[atm_lon_key]]
+            nn_coord = [row[nn_lat_key], row[nn_lon_key]]
+            folium.PolyLine(
+                locations=[atm_coord, nn_coord],
+                color='green',
+                weight=2
+            ).add_to(self.map)
+            folium.Marker(location=atm_coord).add_to(self.map)
+
 
     def save_map(self, outputfile:str):
         self.map.save(outputfile)
