359d0067a059e73c74074941c536e31f2f2f74c4
Sergoot <sergey.lyutavin22@mail.ru>
2024-11-01 14:28:11 +0300

    feat: enable corses in bff

--- bff/bff/main/main.py
+++ bff/bff/main/main.py
@@ -1,5 +1,6 @@
 from typing import Any, Callable
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .config import Settings
 from .di import api_client_factory, compute_route_factory
@@ -16,7 +17,13 @@ def as_dependency(dependency: Any) -> Callable:
 
 def create_app(settings: Settings):
     app = FastAPI()
-    
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     atm_client = api_client_factory(settings.atm_host, settings.atm_port)
     algo_client = api_client_factory(settings.algo_host, settings.algo_port)
     
