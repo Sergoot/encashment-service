d22208382d4d859cb9153a34606c5ab6904e546c
slava <slavabudarin@gmail.com>
2024-11-01 10:05:28 +0300

    актуализировал в соответствии с фолиумУтилс

--- scripts/ATM_to_BD/atms_to_map.py
+++ scripts/ATM_to_BD/atms_to_map.py
@@ -1,3 +1,4 @@
+#скрипт по созданию интерактивной карты с банкоматами с выгрузкой из БД
 import pandas as pd
 from scripts.Utils.PSQLutils import PSQL
 from scripts.Utils.MapUtils.FoliumUtils import ToFoliumMap
@@ -6,10 +7,10 @@ from scripts.Utils.PSQLutils.config import ServerConf, TableATM
 
 sql = PSQL(ServerConf, TableATM)
 lol = sql.fetch_all_rows("in_mkad=TRUE")
+
 sql.close()
 ATM_df = pd.DataFrame(lol, columns=TableATM.table_columns)
-
 output_file = "output_htmls/atms_on_map.html"
 To_map = ToFoliumMap()
-To_map.generate_map(ATM_df,lat_key='x_lon',lon_key='y_lat')
+To_map.generate_atm_map(ATM_df, lat_key='y_lat', lon_key='x_lon')
 To_map.save_map(output_file)
