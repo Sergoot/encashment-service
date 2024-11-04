2f65b5e7633944445a5a8fc6f46ccb8b20587a1e
slava <slavabudarin@gmail.com>
2024-11-04 16:50:41 +0300

    добавил таблицу роутов

--- scripts/Utils/PSQLutils/config.py
+++ scripts/Utils/PSQLutils/config.py
@@ -15,7 +15,7 @@ class ServerConf:
 class TableATM:
     table_name = 'atms'
     table_columns = ['osmid', 'x_lon', 'y_lat', 'operator', 'in_mkad']
-    table_definition = """osmid BIGSERIAL PRIMARY KEY, 
+    table_definition = """  osmid BIGSERIAL PRIMARY KEY, 
                             x_lon float8, 
                             y_lat float8, 
                             operator text, 
@@ -26,7 +26,7 @@ class TableATM:
 class TableNearest:
     table_name = 'nearest_nodes'
     table_columns = ['osmid', 'atm_osmid', 'x_lon', 'y_lat', 'distance_to_atm', 'in_mkad']
-    table_definition = """osmid BIGSERIAL, 
+    table_definition = """  osmid BIGSERIAL, 
                             atm_osmid BIGSERIAL PRIMARY KEY,
                             x_lon float8, 
                             y_lat float8, 
@@ -35,12 +35,37 @@ class TableNearest:
                             FOREIGN KEY (atm_osmid) REFERENCES atms(osmid)"""
 
 
-
-
+class TableRoutes:
+    table_name = 'routes'
+    table_columns = ['direction', 'src', 'dst', 'nodes', 'time', 'distance']
+    table_definition = """  direction TEXT PRIMARY KEY, 
+                            src BIGSERIAL,
+                            dst BIGSERIAL, 
+                            nodes BIGINT[],
+                            time SERIAL,
+                            distance serial"""
+                            #с bigint это шутка какая то
 
 class TestTable:
     #table_name = f'test_table{random.randint(0,100000)}'
     table_name = 'test_table73565'
     #table_columns = '(osmid BIGSERIAL PRIMARY KEY, x_lon float8, y_lat float8, operator text, in_MKAD boolean);'
+    table_columns = ['direction', 'src', 'dst', 'nodes', 'time', 'distance']
+    table_definition = """  direction TEXT PRIMARY KEY, 
+                            src BIGSERIAL,
+                            dst BIGSERIAL, 
+                            nodes BIGINT[],
+                            time SERIAL,
+                            distance serial"""
+
+class TestTable2:
+    #table_name = f'test_table{random.randint(0,100000)}'
+    table_name = 'test_table73565'
+    #table_columns = '(osmid BIGSERIAL PRIMARY KEY, x_lon float8, y_lat float8, operator text, in_MKAD boolean);'
     table_columns = ['osmid', 'x_lon', 'y_lat', 'operator', 'in_mkad']
-    table_definition = 'osmid BIGSERIAL PRIMARY KEY, x_lon float8, y_lat float8, operator text, in_mkad boolean'
+    table_definition = """  osmid BIGSERIAL PRIMARY KEY, 
+                                x_lon float8, 
+                                y_lat float8, 
+                                operator text, 
+                                in_mkad boolean
+                                """
