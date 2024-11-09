213006addccd0a9de94ea35a61e6f18496e208fa
slava <slavabudarin@gmail.com>
2024-11-09 19:36:30 +0300

    переделываю без direction:str

--- scripts/ATM_to_BD/routes_to_db.py
+++ scripts/ATM_to_BD/routes_to_db.py
@@ -6,44 +6,31 @@
 #input('ты уверен???')
 
 from scripts.Utils.PSQLutils.main import PSQL
-from scripts.Utils.PSQLutils.config import TableNearest, ServerConf, TableATM, TableRoutes
+from scripts.Utils.PSQLutils.config import TableNearest, ServerConf, TableATM, TableRoutes2, TableAllowedNNs
 import pandas as pd
 
 from scripts.Utils.MapUtils.OSMUtils import OSMatms
 
-atm_sql = PSQL(ServerConf, TableATM)
-nn_sql = PSQL(ServerConf, TableNearest)
+routes_sql = PSQL(ServerConf, TableRoutes2)
 
-routes_sql = PSQL(ServerConf, TableRoutes)
 
-atms = atm_sql.fetch_rows()
-nn = nn_sql.fetch_rows()
-atms = pd.DataFrame(atms, columns=['atm_osmid' , 'atm_lon', 'atm_lat' , 'operator', 'atm_in_mkad'])
-nn = pd.DataFrame(nn, columns=['nn_osmid', 'atm_osmid' , 'nn_lon', 'nn_lat' , 'distance', 'nn_in_mkad'])
-
-Merged_ATM_NN = pd.merge(atms, nn, on='atm_osmid')
-
-Merged_ATM_NN = Merged_ATM_NN.sort_values(by='distance')
-Merged_ATM_NN = Merged_ATM_NN[(Merged_ATM_NN['atm_in_mkad'] == True) & (Merged_ATM_NN['nn_in_mkad'] == True)]
-Merged_ATM_NN = Merged_ATM_NN.head(1000)
-#Merged_ATM_NN = Merged_ATM_NN.loc[0:1000]
-
+allowed_nn = PSQL(ServerConf,TableAllowedNNs)
+allowed_nn = allowed_nn.fetch_rows()
+allowed_nn = pd.DataFrame(allowed_nn, columns=['osmid'])
 
 
 #atms_in_use = list()
-nn_all = set()
-routes_in_db = routes_sql.fetch_rows()
-routes_in_db = pd.DataFrame(routes_in_db, columns=TableRoutes.table_columns)
-routes_in_db = set(routes_in_db['direction'])
+nn_all = list(allowed_nn['osmid'])
 
-count = 0
-for index, row in Merged_ATM_NN.iterrows():
-    count += 1
-    #atms_in_use.append(row['atm_osmid'])
-    nn_all.add(row['nn_osmid'])
-    if count == 10:
-        #break
-        pass
+print('Инициализация БД с нынешними маршрутами')
+routes_in_db_set = set()
+routes_in_db = routes_sql.fetch_rows()
+routes_in_db = pd.DataFrame(routes_in_db, columns=TableRoutes2.table_columns)
+print('Инициализация завершена')
+
+for index, row in routes_in_db.iterrows():
+    direction = (row['src'],row['dst'])
+    routes_in_db_set.add(direction)
 
 
 routes_all = set()
@@ -51,14 +38,15 @@ for src in nn_all:
     for dst in nn_all:
         if src == dst:
             continue
-        direction = f'{src}->{dst}'
+        direction = (src,dst)
         #direction = (src, dst)
         routes_all.add(direction)
 
-routes_to_calculate = routes_all - routes_in_db
+
+routes_to_calculate = routes_all - routes_in_db_set
 
 print('всего возможных маршрутов: ', len(routes_all))
-print('маршрутов уже в БД: ', len(routes_in_db))
+print('маршрутов уже в БД: ', len(routes_in_db_set))
 print('к вычислению: ',len(routes_to_calculate))
 
 
@@ -68,12 +56,10 @@ ox.init_place_graph()
 print('инициализация завершена')
 count_done = 0
 count_error = 0
-for direction in routes_to_calculate:
+for src,dst in routes_to_calculate:
     #print(direction)
-    src, dst = direction.split('->')
     src = int(src)
     dst= int(dst)
-    #direction = f'{src}->{dst}'
     try:
         nodes = ox.get_shortest_route(src, dst)
         time = ox.get_travel_time(nodes)
