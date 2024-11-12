14f8763ea06eacd4c9ebbf430aa7fef3542ecf35
slavabudarin <slavabudarin@gmail.com>
2024-11-13 00:08:59 +0300

    новый скрипт для создания роутов с маевником

--- scripts/scripts_for_db_write/routes_to_mai_to_db.py
+++ scripts/scripts_for_db_write/routes_to_mai_to_db.py
@@ -1,18 +1,21 @@
 """
-ДАЖЕ НЕ ПЫТАЙСЯ ЭТО ЗАПУСКАТЬ
-я потратил на просчет этого говна 35 часов на стационарном компе
-не оптимизирован, но и скрипт разовый
+копия скрипта по выгрузке всех роутов
+только здесь я просчитываю
+от маи
+к маи
+для всех разрешенных банкоматов
 """
 #input('ты уверен???')
 
 from scripts.Utils.PSQLutils.main import PSQL
-from scripts.Utils.PSQLutils.config import TableNearest, ServerConf, TableATM, TableRoutes2, TableAllowedNNs
+from scripts.Utils.PSQLutils.config import ServerConf, TableAllowedNNs, TableRoutesMAI
 import pandas as pd
 
 from scripts.Utils.MapUtils.OSMUtils import OSMatms
 
-routes_sql = PSQL(ServerConf, TableRoutes2)
+MAI_NN = str(266729320)
 
+routes_sql = PSQL(ServerConf, TableRoutesMAI)
 
 allowed_nn = PSQL(ServerConf,TableAllowedNNs)
 allowed_nn = allowed_nn.fetch_rows()
@@ -25,22 +28,27 @@ nn_all = list(allowed_nn['osmid'])
 print('Инициализация БД с нынешними маршрутами')
 routes_in_db_set = set()
 routes_in_db = routes_sql.fetch_rows()
-routes_in_db = pd.DataFrame(routes_in_db, columns=TableRoutes2.table_columns)
+routes_in_db = pd.DataFrame(routes_in_db, columns=TableRoutesMAI.table_columns)
 print('Инициализация завершена')
 
 for index, row in routes_in_db.iterrows():
-    direction = (row['src'],row['dst'])
+    #direction = (row['src'],row['dst'])
+    direction = row['direction']
     routes_in_db_set.add(direction)
 
 
 routes_all = set()
 for src in nn_all:
-    for dst in nn_all:
-        if src == dst:
-            continue
-        direction = (src,dst)
-        #direction = (src, dst)
-        routes_all.add(direction)
+
+    #direction = (src,dst)
+    direction = f'{src}->MAI'
+    routes_all.add(direction)
+
+for dst in nn_all:
+
+    #direction = (src,dst)
+    direction = f'MAI->{dst}'
+    routes_all.add(direction)
 
 
 routes_to_calculate = routes_all - routes_in_db_set
@@ -56,10 +64,20 @@ ox.init_place_graph()
 print('инициализация завершена')
 count_done = 0
 count_error = 0
-for src,dst in routes_to_calculate:
+#for src,dst in routes_to_calculate:
+
+for direction in routes_to_calculate:
     #print(direction)
+    #src = int(src)
+    #dst= int(dst)
+    src,dst = direction.split('->')
+    if src == 'MAI':
+        src = MAI_NN
+    elif dst == 'MAI':
+        dst = MAI_NN
     src = int(src)
-    dst= int(dst)
+    dst = int(dst)
+
     try:
         nodes = ox.get_shortest_route(src, dst)
         time = ox.get_travel_time(nodes)
@@ -77,8 +95,8 @@ for src,dst in routes_to_calculate:
         src=src,
         dst=dst,
         nodes=nodes,
-        time=time,
-        distance=distance
+        time=int(time),
+        distance=int(distance)
         )
 
 
