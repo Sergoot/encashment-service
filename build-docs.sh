8ef0b4df4d19d107842a24fb6509cd1584fbef3a
Sergoot <sergey.lyutavin22@mail.ru>
2024-12-01 15:15:03 +0300

    test without mkdocs

--- build-docs.sh
+++ build-docs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 lazydocs --output-path="./docs/docstrings" --overview-file="DOCS-BFF.md" --src-base-url="https://github.com/Sergoot/encashment-service/blob/master/" bff
-mkdocs build
+# mkdocs build
